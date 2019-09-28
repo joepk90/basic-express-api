c4ad49b6dcbde94b58d84483106f656f2b224efa
Joe Park-Kennaby <jparkkennaby@gmail.com>
2019-09-28 23:39:14 +0100

    4.8 - setup basic express application with 2 api endpoints. add documentation for using params and query parameters

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+
+const courses = [
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'}
+];
+
+app.get('/', (reg, res) => {
+    res.send('hello world!');
+});
+
+// return all courses
+app.get('/api/courses', (req, res) => {
+    res.send([1,2,3]);
+});
+
+// return course by id
+app.get('/api/courses/:id', (req, res) => {
+
+    // check if the course exists
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+
+    // if course id does not exist return 404
+    if(!course) res.status(404).send('The course with the given ID was not found.')
+
+    // else return the course data
+    res.send(course); 
+
+});
+
+// example date params
+// app.get('/api/posts/:year/:month', (req, res) => {
+//     res.send(req.params);
+// });
+
+// example query
+// app.get('/api/posts', (req, res) => {
+//     res.send(req.query);
+// });
+
+app.post('api/courses/', (req, res) => {
+
+});
+
+
+// TODO install nodemon - then run the application using nodemon index.js
+// setting env variables via terminal - export PORT=5000
+
+// $ export PORT=5000
+const port = process.env.PORT || 3000
+
+app.listen(port, () => console.log(`listening on port ${port}...`))
+// app.post();
+// app.put();
+// app.delete();
